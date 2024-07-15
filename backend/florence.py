27b4ed2c55ad700a7cefee5ae3dc26eb5123d632
matatonic <matatonic-git@zhero.org>
2024-07-15 19:15:50 -0400

    wip

--- backend/florence.py
+++ backend/florence.py
@@ -40,7 +40,10 @@ class VisionQnA(VisionQnABase):
     async def chat_with_images(self, request: ImageChatRequest) -> str:
         images, prompt = await prompt_from_messages(request.messages, self.format)
         
-        inputs = self.processor(text=prompt, images=images[0] if images else None, return_tensors="pt").to(device=self.model.device, dtype=self.model.dtype)
+        if len(images) < 1:
+            images = [ await url_to_image(transparent_pixel_url) ]
+
+        inputs = self.processor(text=prompt, images=images[0], return_tensors="pt").to(device=self.model.device, dtype=self.model.dtype)
 
         default_params = {
             'do_sample': False,
