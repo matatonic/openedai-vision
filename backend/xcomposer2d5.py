a76e7cf07e4b50babb25917cd659f5bec0526c16
matatonic <matatonic-git@zhero.org>
2024-07-16 23:50:15 -0400

    wip

--- backend/xcomposer2d5.py
+++ backend/xcomposer2d5.py
@@ -40,12 +40,10 @@ class VisionQnA(VisionQnABase):
     async def stream_chat_with_images(self, request: ImageChatRequest) -> AsyncGenerator[str, None]:
         prompt, history, files, meta_instruction = await prompt_history_images_system_from_messages(request.messages, img_tok='<ImageHere>', url_handler=url_to_file)
 
-        use_meta = True if meta_instruction else False
-
         if not files:
             files = None
 
-        inputs, im_mask, _ = self.model.interleav_wrap_chat(prompt, files, history=history, meta_instruction=meta_instruction, use_meta=use_meta, hd_num=self.max_tiles)
+        inputs, im_mask, _ = self.model.interleav_wrap_chat(prompt, files, history=history, meta_instruction=meta_instruction, hd_num=self.max_tiles)
 
         inputs = {
             k: v.to(self.device)
